8ace89667d92e9c4fee788c2e846375fabb47425
alk3mik <mozz76@gmail.com>
2017-05-19 23:56:49 +0200

    More tasks added to gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -31,22 +31,54 @@
 		
 */
 
+/*
+	For sass compilation we'll use gulp-sass.
+	NOTE: gulp-sass uses node-sass which in turn uses libsass.
+*/
+
 // grab our gulp packages
 var gulp  = require('gulp'),
     gutil = require('gulp-util'),
-    jshint = require('gulp-jshint');
+    jshint = require('gulp-jshint'),
+    sass  = require('gulp-sass'),
+    // sourcemaps -> to map processed/minified/modified files to their original sources.
+    sourcemaps = require('gulp-sourcemaps');
 
 // define the default task and add the watch task to it
 gulp.task('default', ['watch']);
 
-// configure the jshint task
+// This task will lint our javascript (check for errors) using jshint and it'll
+// set it up to run this task each time we save a javascript file.
 gulp.task('jshint', function() {
   return gulp.src('source/javascript/**/*.js')
     .pipe(jshint())
     .pipe(jshint.reporter('jshint-stylish'));
 });
 
+// Sass Compilation with libsass.
+// Sass serves as a way to extend CSS giving support for variables,
+// nested rules, mixins, inline imports, and more.
+gulp.task('build-css', function() {
+  return gulp.src('source/scss/**/*.scss')
+  	.pipe(sourcemaps.init())  // Process the original sources
+    	.pipe(sass())
+    .pipe(sourcemaps.write()) // Add the map to modified source.
+    .pipe(gulp.dest('public/assets/stylesheets'));
+});
+
+// Javascript concat and minify
+gulp.task('build-js', function() {
+  return gulp.src('source/javascript/**/*.js')
+    .pipe(sourcemaps.init())
+      .pipe(concat('bundle.js'))
+      //only uglify if gulp is ran with '--type production'
+      .pipe(gutil.env.type === 'production' ? uglify() : gutil.noop()) 
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('public/assets/javascript'));
+});
+
 // configure which files to watch and what tasks to use on file changes
 gulp.task('watch', function() {
   gulp.watch('source/javascript/**/*.js', ['jshint']);
+  gulp.watch('source/scss/**/*.scss', ['build-css']);
 });
