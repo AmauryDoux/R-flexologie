dc5dd077143a675931aee626e6a445bbe6b8f61e
ignis33 <julien.leotier@gmail.com>
2017-05-19 20:22:54 +0200

    ficheclients standart avec integration maps et bouton

--- admin/components/home/home.js
+++ admin/components/home/home.js
@@ -9,4 +9,9 @@ angular.module("reflexologie")
 
 function home() {
     this.title = "Ceci sera le home";
+      $(document).ready(function(){
+    // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
+    $('.modal').modal();
+  });
+
 }
