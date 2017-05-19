842d3ed37bb198ce701e355426668b1917c3feea
AmauryDoux <amaury.doux@gmail.com>
2017-05-19 16:41:38 +0200

    updates on the routes

---
+++ admin/app.js
@@ -0,0 +1,17 @@
+'use strict';
+
+var app = angular.module("reflexologie", ["ui.router", "express","bulma"]);
+
+app.config(function($stateProvider, $urlRouterProvider) {
+    var states = [
+        {
+            name: "admin",
+            url: "/admin",
+            component: "admin"
+        }
+    ];
+    $urlRouterProvider.otherwise("/home"); // Page par défaut
+    states.forEach(function(state) {
+        $stateProvider.state(state);
+    });
+});
