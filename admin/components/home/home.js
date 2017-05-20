ec87b925d5016e132170f60da95bcdd66cce0715
ignis33 <julien.leotier@gmail.com>
2017-05-20 15:21:29 +0200

    toast ajouter pour informer user

--- admin/components/home/home.js
+++ admin/components/home/home.js
@@ -3,15 +3,19 @@
 angular.module("reflexologie")
     .component("home", {
         templateUrl: "components/home/home.html",
-        controller : home
+        controller: home
     })
 
 
-function home() {
+function home($scope) {
     this.title = "Ceci sera le home";
-      $(document).ready(function(){
-    // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
-    $('.modal').modal();
-  });
-
+    $(document).ready(function () {
+        $('.modal').modal();
+    });
+    $scope.toast = function toast() {
+        Materialize.toast('Rendez-vous validé, un email est envoyé', 4000)
+    }
+     $scope.nope = function nope() {
+        Materialize.toast('Rendez-vous annulé, un email est envoyé', 4000)
+    }
 }
