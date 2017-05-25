f660e06ba012972d55c1b0d387f468f9f0b79299
ignis33 <julien.leotier@gmail.com>
2017-05-25 17:14:02 +0200

    code couleur pour ficheclient '

--- admin/components/rdv/rdv.js
+++ admin/components/rdv/rdv.js
@@ -8,14 +8,45 @@ angular.module("reflexologie")
 
 
 function Rdv($scope, $resource, $http) {
-    $scope.deleted = function(){
+
+
+    $scope.deleted = function () {
         return $http.delete("/rdv/" + this.rdv._id)
     }
     $scope.toast = function toast() {
         Materialize.toast('Rendez-vous validé, un email est envoyé', 4000)
+        return $http.put('/rdv/' + this.rdv._id, {
+            jour: this.rdv.jour,
+            heureStart: this.rdv.heureStart,
+            heureEnd: this.rdv.heureEnd,
+            patient: {
+                nom: this.rdv.patient.nom,
+                prenom: this.rdv.patient.prenom,
+                email: this.rdv.patient.email,
+                tel: this.rdv.patient.tel,
+                adresse: this.rdv.patient.adresse,
+                commentaire: this.rdv.patient.commentaire
+            },
+            status: 1
+        })
+
     }
     $scope.nope = function nope() {
         Materialize.toast('Rendez-vous annulé, un email est envoyé', 4000)
+         return $http.put('/rdv/' + this.rdv._id, {
+            jour: this.rdv.jour,
+            heureStart: this.rdv.heureStart,
+            heureEnd: this.rdv.heureEnd,
+            patient: {
+                nom: this.rdv.patient.nom,
+                prenom: this.rdv.patient.prenom,
+                email: this.rdv.patient.email,
+                tel: this.rdv.patient.tel,
+                adresse: this.rdv.patient.adresse,
+                commentaire: this.rdv.patient.commentaire
+            },
+            status: 2
+        })
     }
     $(document).ready(function () {
         $('.collapsible').collapsible();
