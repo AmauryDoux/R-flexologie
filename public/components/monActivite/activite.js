b30ad1027e68f32a1d3d0ad1e9ee8ef4bced2976
alk3mik <mozz76@gmail.com>
2017-06-08 16:28:49 +0200

    I have just started this feature...

---
+++ public/components/monActivite/activite.js
@@ -0,0 +1,9 @@
+'use strict';
+
+angular.module("reflexologie")
+    .component("monActivite", {
+        templateUrl: "components/monActivite/activite.html",
+        controller: MonActivite
+    })
+function MonActivite() {
+}
