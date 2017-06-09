65faad250e48ff269641ee51f9dc71099f243db4
AmauryDoux <amaury.doux@gmail.com>
2017-06-09 10:53:05 +0200

    osef

---
+++ public/components/footer/footer.js
@@ -0,0 +1,9 @@
+'use strict';
+
+angular.module("reflexologie")
+    .component("footer", {
+        templateUrl: "components/footer/footer.html",
+        controller: Footer
+    })
+function Footer() {
+}
